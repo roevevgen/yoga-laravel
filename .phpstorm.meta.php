3732150b26eb4673e3bb516d667ff1b8e97e7e97
roevevgen <roevevgen7@gmail.com>
2020-03-17 18:28:44 +0200

    yoga

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-03-15 21:36:21.
+    * Generated on 2020-03-17 11:57:13.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
